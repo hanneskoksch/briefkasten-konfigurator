119ca6e66e2b776931cb1fd0fccff2becc96ff2f
Hannes Koksch <hannes.koksch@gmail.com>
2022-04-07 19:37:23 +0200

    animate bases on hover

--- src/components/configurator/base_color_thumbnail.tsx
+++ src/components/configurator/base_color_thumbnail.tsx
@@ -14,7 +14,7 @@ const BaseColorThumbnail = ({
 }: ColorThumbnailProps) => {
   return (
       <div
-        className={`h-16 w-16 m-1 rounded-full bg-center cursor-pointer ${colorClass} ${
+        className={`w-16 h-16 m-1 transition ease-in-out bg-center rounded-full cursor-pointer hover:scale-105 duration-50 ${colorClass} ${
           baseColor === colorClass && "border-4 border-gray-800"
         }`}
         onClick={() => onClick(colorClass)}
