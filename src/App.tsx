65ac02e0d1529115aeb08541f44f1e51c68cfb2b
Hannes Koksch <hannes.koksch@gmail.com>
2022-12-27 15:07:23 +0100

    outsourced label to own component

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import {
 import { changeColor } from "./slices/colorSlice";
 import { toggleNameField } from "./slices/nameFieldSlice";
 import StampSelectorBoxes from "./components/configurator/stamp_selector_boxes";
+import BetaVersionLabel from "./components/beta_version_label/beta_version_label";
 
 function App() {
   const dispatch = useDispatch();
@@ -82,7 +83,7 @@ function App() {
             />
             <div className="absolute perspective-container">
               <div className=" perspective">
-               <StampsImageOverlay stamps={stampsState} />
+                <StampsImageOverlay stamps={stampsState} />
               </div>
             </div>
             {nameFieldState && (
@@ -108,15 +109,7 @@ function App() {
           <Checkbox checked={nameFieldState} setChecked={onToggleNameField} />
         </div>
       </div>
-
-      <div className="group w-max">
-        <button className="px-2 text-red-600 bg-red-200 border-2 border-red-400 rounded hover:bg-red-300">
-          Beta-Version
-        </button>
-        <span className="absolute px-2 mx-2 text-white transition-opacity bg-black rounded opacity-0 pointer-events-none arrow-left w-max group-hover:opacity-100">
-          Es sind noch nicht alle Stempel und Farben verfügbar.
-        </span>
-      </div>
+      <BetaVersionLabel />
     </div>
   );
 }
