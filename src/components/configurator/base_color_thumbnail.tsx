5ebfd887b36ada186809b90dd4de1473d091cba6
Hannes Koksch <hannes.koksch@gmail.com>
2024-03-17 23:36:05 +0100

    format

--- src/components/configurator/base_color_thumbnail.tsx
+++ src/components/configurator/base_color_thumbnail.tsx
@@ -15,15 +15,13 @@ const BaseColorThumbnail = ({
 }: ColorThumbnailProps) => {
   return (
     <div className="text-center">
-    <div
-      className={`duration-50 m-1 h-16 w-16 cursor-pointer rounded-full bg-center transition ease-in-out hover:scale-105 ${colorClass} ${
-        baseColor === colorClass && "border-4 border-gray-800"
-      }`}
-      onClick={() => onClick(colorClass)}
-    ></div>
-    <p className="text-sm text-slate-800">
-    {name}
-    </p>
+      <div
+        className={`duration-50 m-1 h-16 w-16 cursor-pointer rounded-full bg-center transition ease-in-out hover:scale-105 ${colorClass} ${
+          baseColor === colorClass && "border-4 border-gray-800"
+        }`}
+        onClick={() => onClick(colorClass)}
+      ></div>
+      <p className="text-sm text-slate-800">{name}</p>
     </div>
   );
 };
