f7a42e899aad74bd892fcaf7187573db7d99aa08
Hannes Koksch <hannes.koksch@gmail.com>
2024-10-14 10:57:31 +0200

    replace beta version label by shadcn popover

--- src/components/beta_version_label/beta_version_label.tsx
+++ src/components/beta_version_label/beta_version_label.tsx
@@ -1,13 +1,19 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 const BetaVersionLabel = () => {
   return (
-    <div className="group w-max">
-      <button className="rounded border-2 border-red-400 bg-red-200 px-2 text-red-600 hover:bg-red-300">
+    <Popover>
+      <PopoverTrigger className="rounded border-2 border-red-400 bg-red-200 px-2 text-red-600 hover:bg-red-300">
         Beta-Version
-      </button>
-      <span className="arrow-left pointer-events-none absolute mx-2 w-max rounded bg-black px-2 text-white opacity-0 transition-opacity group-hover:opacity-100">
+      </PopoverTrigger>
+      <PopoverContent side="right">
         Es sind noch nicht alle Stempel und Farben verfügbar.
-      </span>
-    </div>
+      </PopoverContent>
+    </Popover>
   );
 };
 
