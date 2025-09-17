d06a9b24602c678a8e5f40e0ab388b068adefd97
Hannes Koksch <hannes.koksch@gmail.com>
2025-09-17 17:06:26 +0200

    fix erasableSyntaxOnly ts error
    see: https://www.typescriptlang.org/tsconfig/#erasableSyntaxOnly

--- src/utils/enums.tsx
+++ src/utils/enums.tsx
@@ -1,6 +1,8 @@
-export enum BaseColor {
-  Blue = "bg-glazeblue",
-  White = "bg-glazewhite",
-  Grey = "bg-glazegrey",
-  Green = "bg-glazegreen",
-}
+export const BaseColor = {
+  Blue: "bg-glazeblue",
+  White: "bg-glazewhite",
+  Grey: "bg-glazegrey",
+  Green: "bg-glazegreen",
+} as const;
+
+export type BaseColor = typeof BaseColor[keyof typeof BaseColor];
